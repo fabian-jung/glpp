88c7fe7646cfba57746fec19511592c2faf3a707
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2020-04-05 16:37:35 +0200

    Add textbox rendering to writer class and a example using it

--- modules/text/src/charset.cpp
+++ modules/text/src/charset.cpp
@@ -19,9 +19,18 @@ std::vector<charset_t::char_t>::const_iterator charset_t::end() const {
 	return m_chars.end();
 }
 
+charset_t charset_t::digits() {
+	return std::initializer_list<char_t>{L'0', L'1', L'2', L'3', L'4', L'5', L'6', L'7', L'8', L'9' };
+}
+
 charset_t charset_t::ascii() {
-	decltype(m_chars) init(128);
-	std::iota(init.begin(), init.end(), 0);
+	constexpr size_t total =
+		static_cast<size_t>(std::numeric_limits<char>::max()) -
+		static_cast<size_t>(std::numeric_limits<char>::min()) +
+		1;
+
+	decltype(m_chars) init(total);
+	std::iota(init.begin(), init.end(), std::numeric_limits<char_t>::min());
 	return charset_t(std::move(init));
 }
 
