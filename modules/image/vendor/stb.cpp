c217c2c0974ea5a290531e08c8416571fbc37300
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2020-12-26 00:02:26 +0100

    Fix compiler warning when building vendor/stb.cpp

--- modules/image/vendor/stb.cpp
+++ modules/image/vendor/stb.cpp
@@ -1,3 +1,6 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-
+#pragma GCC diagnostic pop
