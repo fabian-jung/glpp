d22b383066c2a62c4e997e4e62548b44027c5b9c
Fabian Jung <fabian.jung@mailbox.tu-dresden.de>
2020-12-26 00:21:44 +0100

    Clean up render::camera_t and add ctors

--- modules/core/include/glpp/render/camera.hpp
+++ modules/core/include/glpp/render/camera.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtx/vector_angle.hpp>
 
 namespace glpp::render {
 
-
-
 struct camera_t {
+
 	glm::vec3 position = {0,0,0};
 	glm::quat orientation = glm::quat(glm::vec3(0.0, 0.0, 0.0));
 	float fov = 90;
@@ -14,19 +14,29 @@ struct camera_t {
 	float far_plane = 100.0f;
 	float aspect_ratio = 1.0f;
 
-	glm::mat4 mvp(const glm::mat4 model_matrix = glm::mat4(1.0f)) const {
-		const glm::mat4 v = glm::translate(model_matrix, -position);
-		const auto r = glm::mat4_cast(orientation);
-		const glm::mat4 p = glm::perspectiveFov(
-			glm::radians(fov),
-												aspect_ratio,
-										1.0f,
-										near_plane,
-										far_plane
-		);
-		return p*r*v;
-	}
+	camera_t() = default;
+
+	camera_t(
+		glm::vec3 position,
+		glm::quat orientation = glm::quat(glm::vec3(0.0, 0.0, 0.0)),
+		float fov = 90,
+		float near_plane = 0.1f,
+		float far_plane = 100.0f,
+		float aspect_ratio = 1.0f
+	);
+
+	camera_t(
+		glm::vec3 position,
+		glm::vec3 lookAt,
+		glm::vec3 up,
+		float fov = 90,
+		float near_plane = 0.1f,
+		float far_plane = 100.0f,
+		float aspect_ratio = 1.0f
+	);
+
+	glm::mat4 mvp(const glm::mat4& model_matrix = glm::mat4(1.0f)) const;
+
 };
 
 }
-
---
+++ modules/core/src/camera.cpp
@@ -0,0 +1,67 @@
+#include <glpp/render/camera.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+glpp::render::camera_t::camera_t(
+	glm::vec3 position,
+	glm::quat orientation,
+	float fov,
+	float near_plane,
+	float far_plane,
+	float aspect_ratio
+) :
+	position(position),
+	orientation(orientation),
+	fov(fov),
+	near_plane(near_plane),
+	far_plane(far_plane),
+	aspect_ratio(aspect_ratio)
+{}
+
+
+glm::quat orientationFromLookAt(const glm::vec3& look_at, const glm::vec3& up) {
+	glm::vec3 forward(0.0f, 0.0f, -1.0f);
+	const auto forward_rotation_axis = glm::normalize(glm::cross(look_at, forward));
+	const float forward_angle = glm::orientedAngle(forward, look_at, forward_rotation_axis);
+	const auto forward_rotation =
+		(forward_rotation_axis==forward_rotation_axis) ?
+			glm::angleAxis(forward_angle, forward_rotation_axis) :
+			glm::quat(glm::vec3(0.0f));
+
+	const auto up_ref = forward_rotation*glm::vec3(0.0f, 1.0f, 0.0f);
+	const auto up_rotation_axis = forward_rotation*glm::vec3(0.0f, 0.0f, 1.0f);
+
+	const float up_angle = glm::orientedAngle(up_ref, up, up_rotation_axis);
+	const auto up_rotation = glm::angleAxis(up_angle, up_rotation_axis);
+
+	return up_rotation*forward_rotation;
+}
+
+glpp::render::camera_t::camera_t(
+	glm::vec3 position,
+	glm::vec3 lookAt,
+	glm::vec3 up,
+	float fov,
+	float near_plane,
+	float far_plane,
+	float aspect_ratio
+) :
+	position(position),
+	orientation(orientationFromLookAt(glm::normalize(lookAt-position), glm::normalize(up-position))),
+	fov(fov),
+	near_plane(near_plane),
+	far_plane(far_plane),
+	aspect_ratio(aspect_ratio)
+{}
+
+glm::mat4 glpp::render::camera_t::mvp(const glm::mat4& model_matrix) const {
+	const glm::mat4 translation = glm::translate(model_matrix, -position);
+	const auto rotation = glm::mat4_cast(glm::inverse(orientation));
+	const glm::mat4 projection = glm::perspectiveFov(
+		glm::radians(fov),
+		aspect_ratio,
+		1.0f,
+		near_plane,
+		far_plane
+	);
+	return projection*rotation*translation;
+}
