b3305ae2ed7c6c921e46de644427399f5b1e2178
Fabian Jung <git@fabian-jung.net>
2021-06-09 23:27:28 +0200

    Fix all the install targets and dependencies

--- glppConfig.cmake
+++ glppConfig.cmake
@@ -5,7 +5,6 @@ set(my-config-var @my-config-var@)
 
 # Same syntax as find_package
 find_dependency(OpenGL REQUIRED COMPONENTS OpenGL)
-find_dependency(GLEW REQUIRED)
 
 find_dependency(PkgConfig REQUIRED)
 pkg_search_module(GLFW REQUIRED glfw3)
@@ -13,4 +12,5 @@ pkg_search_module(GLFW REQUIRED glfw3)
 # Any extra setup
 
 # Add the targets file
+include(${CMAKE_CURRENT_LIST_DIR}/BlenderExport.cmake)
 include("${CMAKE_CURRENT_LIST_DIR}/glppTargets.cmake")
