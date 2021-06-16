2b520ef1ea2ff8dc225c80e4dcb5cfca92d2e394
Fabian Jung <git@fabian-jung.net>
2021-06-16 09:04:16 +0200

    Add vertex_array_t tests

--- modules/core/src/vertex_array.cpp
+++ modules/core/src/vertex_array.cpp
@@ -13,9 +13,29 @@ void vertex_array_t::bind() const {
 	glBindVertexArray(id());
 }
 
+void vertex_array_t::attach_buffer(
+	GLuint binding_point,
+	GLuint index,
+	GLuint elements_per_vertex,
+	GLenum type,
+	GLintptr offset,
+	bool normalized
+) {
+	glEnableVertexArrayAttrib(id(), index);
+	glVertexArrayAttribBinding(id(), index, binding_point);
+	constexpr std::array integral_types = {GL_BYTE, GL_UNSIGNED_BYTE, GL_SHORT, GL_UNSIGNED_SHORT, GL_INT, GL_UNSIGNED_INT};
+	if(type == GL_DOUBLE) {
+		glVertexArrayAttribLFormat(id(), index, elements_per_vertex, type, offset);
+	} else if(std::any_of(integral_types.begin(), integral_types.end(), [type](GLenum v){ return type == v; })){
+		glVertexArrayAttribIFormat(id(), index, elements_per_vertex, type, offset);
+	} else {
+		glVertexArrayAttribFormat(id(), index, elements_per_vertex, type, normalized, offset);
+	}
+}
+
 GLuint vertex_array_t::create() {
 	GLuint id;
-	glGenVertexArrays(1, &id);
+	glCreateVertexArrays(1, &id);
 	return id;
 }
 
