1b3cfdad4e9e8a2bafd86de8e2111133903ab546
Fabian Jung <git@fabian-jung.net>
2021-06-18 00:16:39 +0200

    Add gl_context_t logging and mock_context_t

--- modules/gl/bin/generator.cpp
+++ modules/gl/bin/generator.cpp
@@ -131,6 +131,7 @@ R"(
 #pragma once
 #include "types.hpp"
 #include <functional>
+#include <ostream>
 
 #ifndef WITH_GLEW
 
@@ -147,6 +148,7 @@ struct gl_context_t {
     gl_context_t() = default;
 
     void enable_throw();
+    void enable_logging(std::ostream& out);
 
     template <class Function>
     void for_each(Function&& fun);
@@ -163,6 +165,10 @@ struct gl_context_t {
 R"(    
 };
 inline thread_local gl_context_t context;
+
+struct mock_context_t : public gl_context_t {
+    mock_context_t();
+};
 
 template <class Function>
 void gl_context_t::for_each(Function&& function) {
@@ -233,6 +239,51 @@ void gl_context_t::enable_throw() {
         }
     });
 
+}
+
+mock_context_t::mock_context_t() {
+    for_each([](auto& fn, const char*, int) {
+        fn = [](auto... args) -> decltype(fn(args...)) {
+            using result_t = decltype(fn(args...));
+            if constexpr(!std::is_same_v<result_t, void>) {
+                return result_t{};
+            }
+        };
+    });
+}
+
+template <class... Args>
+void print(std::ostream& out, Args... args);
+
+template <>
+void print(std::ostream&) {}
+
+template <class T>
+void print(std::ostream& out, T v) {
+    out << v << ":" << typeid(T).name();
+}
+
+template <class T, class U, class... Args>
+void print(std::ostream& out, T v, U w, Args... args) {
+    print(out,v);
+    out << ", ";
+    print(out, w, args...);
+}
+
+void gl_context_t::enable_logging(std::ostream& out) {
+
+    for_each([&out](auto& fn, const char* name, int) {
+        fn = [&out, f = std::move(fn), name](auto... args) -> decltype(fn(args...)) {
+            out << name << "(";
+            print(out, args...);
+            out << ")\n";
+            if constexpr(std::is_same_v<decltype(fn(args...)), void>) {
+                f(args...);
+            } else {
+                return f(args...);
+            }
+        };
+    });
 }
 
 }
