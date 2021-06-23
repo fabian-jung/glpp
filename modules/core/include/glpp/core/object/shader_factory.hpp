7658e3e8651b7a9223543456adf13cc54c18d949
Fabian Jung <git@fabian-jung.net>
2021-06-24 00:20:24 +0200

    Minor fixes in shader_factory.hpp

--- modules/core/include/glpp/core/object/shader_factory.hpp
+++ modules/core/include/glpp/core/object/shader_factory.hpp
@@ -1,19 +1,26 @@
 #pragma once
 
 #include <string>
+#include <istream>
 #include <fstream>
+#include <sstream>
 
 namespace glpp::core::object {
 
 class shader_factory_t {
 public:
 	explicit shader_factory_t(std::ifstream& file);
-	explicit shader_factory_t(std::ifstream file);
+	explicit shader_factory_t(std::ifstream&& file);
+
+	explicit shader_factory_t(const std::stringstream& sstream);
+
 	explicit shader_factory_t(std::string code_template);
 	
 	template <class T>
 	shader_factory_t& set(std::string_view key, const T& value);
 	
+	shader_factory_t& set(std::string_view key, const char* value);
+
 	std::string code() const {
 		return m_code;
 	}
