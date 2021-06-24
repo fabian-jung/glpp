141674079653d93b4b2b9fbe0a9316088d16bda2
Fabian Jung <git@fabian-jung.net>
2021-06-24 20:46:14 +0200

    Extend CI pipeline to run tests against the installed version of glpp

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -18,6 +18,38 @@ test_fn() {
 	ctest
 }
 
+install_fn() {
+	make install
+}
+
+post_install_test_fn() {
+	cd /test
+	cp -r /glpp/test .
+
+	cat > CMakeLists.txt << EOF
+	    cmake_minimum_required(VERSION 3.16)
+		project(glpp-tests)
+
+		find_package(Catch2 REQUIRED)
+		include(CTest NO_POLICY_SCOPE)
+		include(Catch NO_POLICY_SCOPE)
+		include(ParseAndAddCatchTests)
+
+		find_package(glpp REQUIRED)
+
+		set(enable_unit_test TRUE)
+
+		add_subdirectory(test)
+EOF
+	rm -rf build
+	mkdir build
+
+	cd build
+	cmake ..
+	make -j
+	ctest
+}
+
 if [[ "$1" == "info" ]]; then
 	info_fn
 elif [[ "$1" == "build" ]]; then
@@ -25,6 +57,9 @@ elif [[ "$1" == "build" ]]; then
 elif [[ "$1" == "test" ]]; then
 	build_fn
 	test_fn
+	install_fn
+	post_install_test_fn
+
 elif [[ "$1" == "bash" ]]; then
 	/bin/bash
 else
