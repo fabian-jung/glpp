c7895fc77d67709c4597602fef359f002cfcd76a
Fabian Jung <git@fabian-jung.net>
2021-07-07 21:46:36 +0200

    texture_atlas_t rework

--- modules/core/include/glpp/core/object/texture_atlas.hpp
+++ modules/core/include/glpp/core/object/texture_atlas.hpp
@@ -13,7 +13,10 @@ template <class AllocationPolicy>
 class texture_atlas_t {
 public:
 
-	texture_atlas_t() = default;
+	template <class... Args>
+	texture_atlas_t(const Args... args) :
+		m_alloc(args...)
+	{}
 	
 	texture_atlas_t(texture_atlas_t&& mov) noexcept = default;
 	texture_atlas_t(const texture_atlas_t& cpy) = delete;
@@ -22,33 +25,64 @@ public:
 	texture_atlas_t& operator=(const texture_atlas_t& cpy) = delete;
 	
 	using key_t = typename AllocationPolicy::key_t;
+	using key_hint_t = typename AllocationPolicy::key_hint_t;
 
+	template <class PixelFormat>
+	key_t alloc(const image_t<PixelFormat>& image) {
+		return alloc(m_alloc.first_free_key(), image);
+	}
+
+	template <class PixelFormat>
+	key_t alloc(const key_hint_t key, const image_t<PixelFormat>& image) {
+		return m_alloc.alloc(key, image);
+	}
+
+	key_t alloc(const key_hint_t key_hint) {
+		return alloc(key_hint, image_t<glm::vec3>{1, 1});
+	}
+
+	key_t alloc() {
+		return alloc(m_alloc.first_free_key());
+	}
 
 	template <class Image>
-	key_t alloc(const Image& image);
-                      	
-	template <class Image>
-	void update(key_t key, const Image& image);
+	void update(const key_t key, const Image& image) {
+		m_alloc.update(key, image);
+	}
 
-	std::string declaration() const;
-	std::string fetch(const key_t key, const std::string_view uv) const;
-	std::string fetch(std::string_view key, const std::string_view uv) const;
-	
+	void free(const key_t key) {
+		m_alloc.free(key);
+	}
+
+	auto keys() const {
+		return m_alloc.keys();
+	}
+
+	std::string declaration(const std::string_view name) const {
+		return m_alloc.declaration(name);
+	}
+
+	std::string fetch(const std::string_view name, const key_t key, const std::string_view uv) const {
+		return fetch(name, std::to_string(key), uv);
+	}
+
+	std::string fetch(const std::string_view name, const std::string_view key, const std::string_view uv) const {
+		return m_alloc.fetch(name, key, uv);
+	}
+
+	std::string fetch(const std::string_view name, const key_t key, const glm::vec2 uv) const {
+		return fetch(name, std::to_string(key), uv);
+	}
+
+	std::string fetch(const std::string_view name, const std::string_view key, const glm::vec2 uv) const {
+		return fetch(name, key, fmt::format("vec2({}, {})", uv.x, uv.y));
+	}
 
 	texture_atlas_slot_t<AllocationPolicy> bind_to_texture_slot() const;
 
-	std::string texture_id() const;
-
 private:
 
-	std::string texture_id(const key_t key) const {
-		return m_namespace+"["+std::to_string(key)+"]";
-	}
-	std::string m_namespace = "texa";
-	
-	using texture_storage_t = typename AllocationPolicy::storage_t;
-	texture_storage_t m_storage;
-	
+	AllocationPolicy m_alloc;		
 };
 
 template <class AllocationPolicy>
@@ -65,62 +99,36 @@ private:
 	slot_storage_t m_slots;
 };
 
-template <class AllocationPolicy>
-template <class Image>
-typename texture_atlas_t<AllocationPolicy>::key_t 
-texture_atlas_t<AllocationPolicy>::alloc(const Image& image) {
-	return AllocationPolicy::alloc(m_storage, image);
-}
 
-template <class AllocationPolicy>					
-template <class Image>
-void texture_atlas_t<AllocationPolicy>::update(key_t key, const Image& image) {
-	AllocationPolicy::update(m_storage, key, image);
-}
+// template <class AllocationPolicy>
+// texture_atlas_slot_t<AllocationPolicy> texture_atlas_t<AllocationPolicy>::bind_to_texture_slot() const {
+// 	return texture_atlas_slot_t<AllocationPolicy>{ m_storage };
+// }
 
-template <class AllocationPolicy>
-std::string texture_atlas_t<AllocationPolicy>::declaration() const {
-	std::string result;
-	if(AllocationPolicy::size(m_storage) > 0) {
-		result += "uniform sampler2D "+texture_id()+"["+std::to_string(m_storage.size())+"];\n";
-	}
-	return result;
-}
+// template <class AllocationPolicy>
+// std::string texture_atlas_t<AllocationPolicy>::texture_id() const {
+// 	return m_namespace;
+// }
 
-template <class AllocationPolicy>
-std::string texture_atlas_t<AllocationPolicy>::fetch(const key_t key, const std::string_view uv) const {
-	return "texture("+texture_id(key)+", "+std::string(uv)+")";
-}
+// template <class AllocationPolicy>
+// texture_atlas_slot_t<AllocationPolicy>::texture_atlas_slot_t(const typename AllocationPolicy::storage_t& storage) :
+// 	m_slots(AllocationPolicy::slots_from_storage(storage))
+// {}
 
-template <class AllocationPolicy>
-texture_atlas_slot_t<AllocationPolicy> texture_atlas_t<AllocationPolicy>::bind_to_texture_slot() const {
-	return texture_atlas_slot_t<AllocationPolicy>{ m_storage };
-}
+// template <class AllocationPolicy>
+// texture_atlas_slot_t<AllocationPolicy>::texture_atlas_slot_t(const texture_atlas_t<AllocationPolicy>& atlas) :
+// 	texture_atlas_slot_t(atlas.bind_to_texture_slot())
+// {}
 
-template <class AllocationPolicy>
-std::string texture_atlas_t<AllocationPolicy>::texture_id() const {
-	return m_namespace;
-}
+// template <class AllocationPolicy>
+// auto texture_atlas_slot_t<AllocationPolicy>::begin() const {
+// 	return m_slots.begin();
+// }
 
-template <class AllocationPolicy>
-texture_atlas_slot_t<AllocationPolicy>::texture_atlas_slot_t(const typename AllocationPolicy::storage_t& storage) :
-	m_slots(AllocationPolicy::slots_from_storage(storage))
-{}
-
-template <class AllocationPolicy>
-texture_atlas_slot_t<AllocationPolicy>::texture_atlas_slot_t(const texture_atlas_t<AllocationPolicy>& atlas) :
-	texture_atlas_slot_t(atlas.bind_to_texture_slot())
-{}
-
-template <class AllocationPolicy>
-auto texture_atlas_slot_t<AllocationPolicy>::begin() const {
-	return m_slots.begin();
-}
-
-template <class AllocationPolicy>
-auto texture_atlas_slot_t<AllocationPolicy>::end() const {
-	return m_slots.end();
-}
+// template <class AllocationPolicy>
+// auto texture_atlas_slot_t<AllocationPolicy>::end() const {
+// 	return m_slots.end();
+// }
 
 // using freefloat_atlas_t = texture_atlas_t<texture_atlas::freefloat_policy_t>;
 // extern template class texture_atlas_t<texture_atlas::freefloat_policy_t>;
