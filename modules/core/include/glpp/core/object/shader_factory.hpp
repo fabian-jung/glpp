4ae97c9e10ee3b03db1fc6a7cc8f6c4e5c95589f
Fabian Jung <git@fabian-jung.net>
2021-07-28 21:17:01 +0200

    minor fixes

--- modules/core/include/glpp/core/object/shader_factory.hpp
+++ modules/core/include/glpp/core/object/shader_factory.hpp
@@ -16,14 +16,19 @@ public:
 
 	explicit shader_factory_t(std::string code_template);
 	
+	shader_factory_t(const shader_factory_t& cpy) = default;
+	shader_factory_t(shader_factory_t&& mov) noexcept = default;
+
+	shader_factory_t& operator=(const shader_factory_t& cpy) = default;
+	shader_factory_t& operator=(shader_factory_t&& mov) noexcept = default;
+
 	template <class T>
 	shader_factory_t& set(std::string_view key, const T& value);
 	
 	shader_factory_t& set(std::string_view key, const char* value);
 
-	std::string code() const {
-		return m_code;
-	}
+	std::string code() const ;
+	
 	
 private:
 	std::string m_code;
